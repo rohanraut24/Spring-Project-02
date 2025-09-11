d78823519c304b747ddab6095d5d5dd64db6d110
rohan raut <rohanraut2443@gmail.com>
2025-09-11 21:34:24 +0530

    start testing data

--- 01-EmployeeManagement/src/test/java/myCode/EmployeeManagementTests.java
+++ 01-EmployeeManagement/src/test/java/myCode/EmployeeManagementTests.java
@@ -1,13 +1,27 @@
 package myCode;
 
+import myCode.dto.EmployeeDto;
+import myCode.model.Employee;
+import myCode.repository.EmployeeRepo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class EmployeeManagementTests {
 
+    @Autowired
+    public EmployeeRepo employeeRepo;
+
 	@Test
 	void contextLoads() {
-	}
+        List<Employee> list = employeeRepo.findAll();
+        System.out.println(list);
+
+//        Employee emp = new Employee("Ronnie","email@g.com");
+//        System.out.println((employeeRepo.save(emp)));
+    }
 
 }
