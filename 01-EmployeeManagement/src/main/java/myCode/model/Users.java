6053b6066d6fc3ff16af01c9913c8463aedc3c6a
rohan raut <rohanraut2443@gmail.com>
2025-09-16 16:45:34 +0530

    starting new project

--- 01-EmployeeManagement/src/main/java/myCode/model/Users.java
+++ 01-EmployeeManagement/src/main/java/myCode/model/Users.java
@@ -1,4 +1,20 @@
 package myCode.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+import org.mapstruct.ap.internal.model.GeneratedType;
+
+@Data
+@ToString(exclude = "password")
+@AllArgsConstructor
 public class Users {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Integer id;
+    private Integer username;
+    private Integer password;
 }
