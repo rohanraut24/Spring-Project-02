3a842ee9e7ff556c17331137da6f4112026108f9
rohan raut <rohanraut2443@gmail.com>
2025-09-30 22:58:37 +0530

    docker setup almost done

--- 02-AdvanceTodosBackend/src/main/java/rohan/dto/user/request/ChangePasswordRequest.java
+++ 02-AdvanceTodosBackend/src/main/java/rohan/dto/user/request/ChangePasswordRequest.java
@@ -10,6 +10,6 @@ public class ChangePasswordRequest {
     private String currentPassword;
 
     @NotBlank(message = "New password is required")
-    @Size(min = 6, max = 40, message = "New password must be between 6 and 40 characters")
+    @Size(min = 4, max = 40, message = "New password must be between 6 and 40 characters")
     private String newPassword;
 }
